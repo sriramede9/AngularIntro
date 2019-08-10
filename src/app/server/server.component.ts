b7e74e0215d7a8f6d57a75ab0d2f8196f68b18b7
sriramede9 <only4programming03@gmail.com>
2019-08-10 10:16:27 -0400

    udenrstanding databinding and eventhandling

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -1,13 +1,17 @@
-import { Component } from "@angular/core";
+import { Component } from '@angular/core';
 
 @Component({
-  selector: "app-server",
-  templateUrl: "./server.component.html",
-  styleUrls: ["./server.component.css"]
+  selector: 'app-server',
+  templateUrl: './server.component.html',
+  styleUrls: ['./server.component.css']
 })
 export class ServerComponent {
   //going to add this in app component
   //add declarator a ts feature which allow to enhance to use
-  serverId:number=9;
-  serverStatus:string='offline';
+  serverId: number = 9;
+  serverStatus: string = 'offline';
+
+  getServerStatus() {
+    return 'offline';
+  }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,11 +2,22 @@ import { Component, OnInit } from "@angular/core";
 
 @Component({
   selector: "app-servers",
-  template: " <app-server></app-server> ",
+  templateUrl: "./servers.component.html",
   styleUrls: ["./servers.component.css"]
 })
 export class ServersComponent implements OnInit {
-  constructor() {}
+  allowNewServer: boolean = false;
+  serverstatusCreation = "no server is created";
+
+  constructor() {
+    setTimeout(() => {
+      this.allowNewServer = true;
+    }, 2000);
+  }
 
   ngOnInit() {}
+
+  onCreateServer() {
+    this.serverstatusCreation = "Server is Created";
+  }
 }
